fa2ec78105d64ea39c68df14381c27805cb647bb
hensley-jack <jh652123@ohio.edu>
2025-11-08 17:44:27 -0500

    Added descriptions for all test cases

--- test_stringcalculator.cpp
+++ test_stringcalculator.cpp
@@ -2,61 +2,61 @@
 #include "catch_amalgamated.hpp"
 
 TEST_CASE("Integer checker works", "[isInteger]") {
-    REQUIRE(isInteger("3.14159") == false); //base
-    REQUIRE(isInteger("0") == true); //edge
-    REQUIRE_THROWS_AS(isInteger("three.five"), std::invalid_argument); //error case
+    REQUIRE(isInteger("3.14159") == false); //base - checks non integer
+    REQUIRE(isInteger("0") == true); //edge - checks zero
+    REQUIRE_THROWS_AS(isInteger("three.five"), std::invalid_argument); //error case - checks non-numerical
 }
 
 TEST_CASE("Sign checker works", "[isPositive]") {
-    REQUIRE(isPositive("-5") == false); //base
+    REQUIRE(isPositive("-5") == false); //base - checks negative
     REQUIRE(isPositive("0") == false); //edge (0 is first non positive value, in descending order)
-    REQUIRE_THROWS_AS(isPositive("zero"), std::invalid_argument); //error case
+    REQUIRE_THROWS_AS(isPositive("zero"), std::invalid_argument); //error case - checks non-numerical
 }
 
 TEST_CASE("Modulus works", "[modulus]") {
-    REQUIRE(modulus("10", "3") == 1); //base
-    REQUIRE(modulus("10", "2") == 0); //edge
+    REQUIRE(modulus("10", "3") == 1); //base - checks modulo with remainder
+    REQUIRE(modulus("10", "2") == 0); //edge - checks modulo without remainder
     REQUIRE_THROWS_AS(modulus("3", "0"), std::invalid_argument); //error case (cannot divide(or mod) by 0)
 }
 
 TEST_CASE("Exponent function works", "[power]") {
-    REQUIRE(power("2", "2") == 4); //base
-    REQUIRE(power("10", "-2") == 0.01); //edge (negative exponent)
-    REQUIRE_THROWS_AS(power("ten", "one"), std::invalid_argument); //error case
+    REQUIRE(power("2", "2") == 4); //base - checks positive integer to a positive exponent
+    REQUIRE(power("10", "-2") == 0.01); //edge - checks positive integer to a negative exponent
+    REQUIRE_THROWS_AS(power("ten", "one"), std::invalid_argument); //error case - checks non-numerical
 }
 
 TEST_CASE("SquareRoot works", "[squareRoot]") {
-    REQUIRE(squareRoot("100") == 10); //base
+    REQUIRE(squareRoot("100") == 10); //base - takes squareroot of pos int
     REQUIRE(squareRoot("0") == 0); //edge (cannot take sqrt of less than 0)
     REQUIRE_THROWS_AS(squareRoot("-100"), std::invalid_argument); //error (sqrt of negative)
 }
 
 TEST_CASE("Squaring works", "[square]") {
-    REQUIRE(square("2") == 4); //base
-    REQUIRE(square("-10") == 100); //edge
-    REQUIRE_THROWS_AS(square("abcd"), std::invalid_argument); //error case
+    REQUIRE(square("2") == 4); //base - checks squaring of a pos int
+    REQUIRE(square("-10") == 100); //edge - checks squaring of negative int
+    REQUIRE_THROWS_AS(square("abcd"), std::invalid_argument); //error case - checks non-numerical
 }
 
 TEST_CASE("Division works", "[divide]") {
-    REQUIRE(divide("200", "20") == 10); //base
-    REQUIRE(divide("1000", "-100") == -10); //edge
+    REQUIRE(divide("200", "20") == 10); //base - checks division of pos int by pos int
+    REQUIRE(divide("1000", "-100") == -10); //edge - checks division of pos int by negative int
     REQUIRE_THROWS_AS(divide("999", "0"), std::invalid_argument); //error case (divide by 0)
 }
 
 TEST_CASE("Multiplication works", "[multiply]") {
-    REQUIRE(multiply("10", "9") == 90); //base
+    REQUIRE(multiply("10", "9") == 90); //base - checks multiplication of pos int by pos int
     REQUIRE(multiply("-20", "222") == -4440); //edge (multiplying by a negative)
-    REQUIRE_THROWS_AS(multiply("one", "one"), std::invalid_argument); //error case
+    REQUIRE_THROWS_AS(multiply("one", "one"), std::invalid_argument); //error case - checks non-numerical
 }
 
 TEST_CASE("Subtraction works", "[subtract]") {
-    REQUIRE(subtract("100", "40") == 60); //base
+    REQUIRE(subtract("100", "40") == 60); //base - checks subtraction of pos int by pos int
     REQUIRE(subtract("100", "-20") == 120); //edge (subtracting a negative)
-    REQUIRE_THROWS_AS(subtract("ten", "two"), std::invalid_argument); //error case
+    REQUIRE_THROWS_AS(subtract("ten", "two"), std::invalid_argument); //error case - checks non-numerical
 }
 
 TEST_CASE("Addition works", "[add]") {
-    REQUIRE(add("6", "7") == 13); //base
-    REQUIRE(add("-6", "7") == 1); //edge (negative)
-    REQUIRE_THROWS_AS(add("abc", "def"), std::invalid_argument); //error case (non numerical)
+    REQUIRE(add("6", "7") == 13); //base - checks addition of two pos ints
+    REQUIRE(add("-6", "7") == 1); //edge - checks addition of negative int and positive int
+    REQUIRE_THROWS_AS(add("abc", "def"), std::invalid_argument); //error case - checks non-numerical
 }
